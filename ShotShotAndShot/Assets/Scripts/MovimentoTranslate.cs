acc29123f14689a9ec9fe624d1575526aefda0f5
Beatriz Teixeira <akemy.coffee.b@gmail.com>
2025-04-19 17:01:06 -0300

    Update MovimentoTranslate.cs

--- ShotShotAndShot/Assets/Scripts/MovimentoTranslate.cs
+++ ShotShotAndShot/Assets/Scripts/MovimentoTranslate.cs
@@ -7,21 +7,14 @@ public class MovimentoTranslate : MonoBehaviour
 {
     public float velocidade = 5f;
     float movimentoX = 0;
-    float DirX = 0;
     Animator anim;
-    bool Andando, Idle;
-    bool Vira = true;
-    
-
-
+    bool Andando;
     void Start()
     {
         anim = GetComponent<Animator>();
     }
     void Update()
     {
-        
-        //Direcao();
         if (movimentoX != 0){
             Andando = true;
             anim.SetBool("Andando", Andando);
@@ -37,30 +30,14 @@ public class MovimentoTranslate : MonoBehaviour
         movimentoX = -1;
         scale.x = -1;
         transform.localScale = scale;
-        //DirX = -1;
     }
     public void Direita(){
         Vector3 scale = transform.localScale;
         movimentoX = 1;
         scale.x = 1;
         transform.localScale = scale;
-        //DirX = 1;
     }
     public void Parar(){
         movimentoX = 0;
     }
-    /*
-    void Direcao(){
-        if (Vira && DirX < 0){
-            Virar();
-        }
-        if (!Vira && DirX > 0){
-            Virar();
-        }
-    }
-    void Virar(){
-        Vira = !Vira;
-        transform.Rotate(0,180,0);
-    }
-    */
 }
