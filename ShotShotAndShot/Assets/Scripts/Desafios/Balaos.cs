aac149ea9a21b968f0914988d451ddb8ed884a16
Beatriz Teixeira <akemy.coffee.b@gmail.com>
2025-04-19 20:16:29 -0300

    BaloesSubindo

--- ShotShotAndShot/Assets/Scripts/Desafios/Balaos.cs
+++ ShotShotAndShot/Assets/Scripts/Desafios/Balaos.cs
@@ -4,15 +4,23 @@ using UnityEngine;
 
 public class Balaos : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
+    public float CimaVel = 0.05f;
     void Update()
     {
-        
+
     }
+    private void OnMouseDown()
+    {
+        ResetarPosicao();
+    }
+    private void FixedUpdate()
+    {
+        transform.Translate(0, CimaVel, 0);
+    }
+    private void ResetarPosicao(){
+        float randomX = Random.Range(-9.4f, 9.4f);
+
+        transform.position = new Vector2(randomX, -8f);
+    }
+    
 }
