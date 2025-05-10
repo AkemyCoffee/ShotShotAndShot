b35671d4f7e6932d4006280d8c0ae542187efc71
Beatriz Teixeira <akemy.coffee.b@gmail.com>
2025-05-10 13:50:50 -0300

    Update Obstáculos.cs

--- ShotShotAndShot/Assets/Scripts/Desafios/Corrida/Obstáculos.cs
+++ ShotShotAndShot/Assets/Scripts/Desafios/Corrida/Obstáculos.cs
@@ -7,9 +7,9 @@ using UnityEngine.UIElements;
 
 public class Obstáculos : MonoBehaviour
 {
-    public float VelBaixo = 0.1f;
+    public float VelBaixo = 0.12f;
     public float randomX, crescendo = 1f;
-    public GameObject OutroObstaculo;
+    //public GameObject OutroObstaculo;
     public Sprite[] SprObs;
     SpriteRenderer sr;
     void Start()
@@ -18,10 +18,12 @@ public class Obstáculos : MonoBehaviour
     }
     void Update()
     {
+        if(gameObject.name == "Obstaculo"){
+            StartCoroutine(Espere(2));
+        }
         if (transform.position.y < -4){
             sr.sortingOrder = 2;
         }
-        StartCoroutine(Espere(2));
         Vector3 crescer = Vector3.one * crescendo* Time.deltaTime;
         if(transform.position.y< -10){   
             ResetarPosicao();
@@ -54,8 +56,9 @@ public class Obstáculos : MonoBehaviour
         sr.sortingOrder = -2;
     }
     IEnumerator Espere(int segundos){
+        VelBaixo = 0;
         yield return new WaitForSeconds(segundos);
-        OutroObstaculo.SetActive(true);
+        VelBaixo =  0.12f;
     }
     
 }
